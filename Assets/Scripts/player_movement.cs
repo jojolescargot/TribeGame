7b1ca6ab06908810f55164cbb879f54689d438cf
jojolescargot <79799891+jojolescargot@users.noreply.github.com>
2023-03-12 23:55:05 +0100

    Add king movement + move effect  but need to ad something to make the digonals better

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
-    public Vector2 move;
-    public float moveSpeed = 20;
+    public GameObject DeplacementEffectPrefab;
+    public Vector2 move = new Vector2 ( 0, 0);
     Rigidbody2D body;
-
+    public double smoothing = 1f;
+    Vector3 targetPos;
+    public bool canMove = true;
+    bool inMove = false;
+    Vector3 lockedTarget;
+    Quaternion rotation;
+    float angle;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +26,66 @@ public class player_movement : MonoBehaviour
         // get input
         move.x = Input.GetAxisRaw("Horizontal"); 
         move.y = Input.GetAxisRaw("Vertical");
-        move.Normalize();        
+        move.x = move.x * 0.225f;
+        move.y = move.y * 0.225f;
+        targetPos = transform.position + new Vector3 (move.x, move.y , 0);
+
     }
     void FixedUpdate()
     {
-        // adjust object velocity
-        body.velocity = body.velocity + move * Time.fixedDeltaTime * moveSpeed;
+        if( transform.position == lockedTarget)
+        {
+            inMove = false;
+        }
+        if(canMove & move != new Vector2(0,0))
+        {
+            // Right or left
+            if(move ==  new Vector2(0.225f,0))
+            {
+                angle = 0;
+            }else if(move ==  new Vector2(-0.225f,0))
+            {
+                angle = 180;
+            }
+            // Top or down
+            else if(move ==  new Vector2(0, -0.225f))
+            {
+                angle = -90;
 
+            }else if(move ==  new Vector2(0, 0.225f))
+            {
+                angle = 90;
+            }
+            // Diagonals
+            else if(move ==  new Vector2(0.225f, 0.225f))
+            {
+                angle = 45;
+            }else if(move ==  new Vector2(0.225f, -0.225f))
+            {
+                angle = -45;
+            }else if(move ==  new Vector2(-0.225f, -0.225f)) 
+            {
+                angle = -135;
+            }else if(move ==  new Vector2(-0.225f, 0.225f))
+            {
+                angle = 135;
+            }
+
+
+            rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            if((move ==  new Vector2(-0.225f,0)))
+            {
+                rotation = rotation * new Quaternion(180,0,0,0);
+            }
+            Instantiate(DeplacementEffectPrefab, transform.position, rotation);
+            inMove = true;
+            canMove = false;
+            lockedTarget = targetPos;
+        }
+        if(inMove)
+        {
+            transform.position = Vector3.Lerp(transform.position, lockedTarget, (float)smoothing * Time.deltaTime);
+        }
     }
 }
+ 
