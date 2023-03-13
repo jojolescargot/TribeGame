1e126808ff2103f02c27b1ed4d51d4aa36fba057
jojolescargot <79799891+jojolescargot@users.noreply.github.com>
2023-03-13 22:23:23 +0100

    Add Diagonals but didnt look really good maybe need to do this an other way and there are bug with the movementEffect

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class player_movement : MonoBehaviour
 {
     public GameObject DeplacementEffectPrefab;
-    public Vector2 move = new Vector2 ( 0, 0);
-    Rigidbody2D body;
     public double smoothing = 1f;
     Vector3 targetPos;
     public bool canMove = true;
@@ -14,78 +12,111 @@ public class player_movement : MonoBehaviour
     Vector3 lockedTarget;
     Quaternion rotation;
     float angle;
+    float x;
+    float y;
+    public float frameNeedForMovement = 10;
+    int frameCounter = 0;
+    public Vector2 move;
+
     // Start is called before the first frame update
     void Start()
     {
-        body = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
     }
 
     // Update is called once per frame
     void Update()
     {
         // get input
-        move.x = Input.GetAxisRaw("Horizontal"); 
-        move.y = Input.GetAxisRaw("Vertical");
-        move.x = move.x * 0.225f;
-        move.y = move.y * 0.225f;
-        targetPos = transform.position + new Vector3 (move.x, move.y , 0);
+        if(Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Vertical") == -1 || Input.GetAxisRaw("Horizontal") == -1 && !inMove)
+        {
+            frameCounter += 1;
+        }
+        if (frameCounter == frameNeedForMovement)
+        {
+            move.x = Input.GetAxisRaw("Horizontal"); 
+            move.y = Input.GetAxisRaw("Vertical");
+            move.x = move.x * 0.225f;
+            move.y = move.y * 0.225f;
+            targetPos = transform.position + new Vector3 (move.x, move.y , 0);
+            frameCounter = 0;
+        }
+        
+
+
 
     }
+    
+
     void FixedUpdate()
     {
+
         if( transform.position == lockedTarget)
         {
             inMove = false;
+            move = Vector2.zero;
+            lockedTarget = Vector3.zero;
+            targetPos =    Vector3.zero;
+            canMove = true;
+
         }
+        
         if(canMove & move != new Vector2(0,0))
         {
             // Right or left
-            if(move ==  new Vector2(0.225f,0))
+            if(move ==  new Vector2(1,0))
             {
                 angle = 0;
-            }else if(move ==  new Vector2(-0.225f,0))
+            }else if(move ==  new Vector2(-1,0))
             {
                 angle = 180;
             }
             // Top or down
-            else if(move ==  new Vector2(0, -0.225f))
+            else if(move ==  new Vector2(0, -1))
             {
                 angle = -90;
 
-            }else if(move ==  new Vector2(0, 0.225f))
+            }else if(move ==  new Vector2(0, 1))
             {
                 angle = 90;
             }
             // Diagonals
-            else if(move ==  new Vector2(0.225f, 0.225f))
+            else if(move ==  new Vector2(1, 1))
             {
                 angle = 45;
-            }else if(move ==  new Vector2(0.225f, -0.225f))
+            }else if(move ==  new Vector2(1, -1))
             {
                 angle = -45;
-            }else if(move ==  new Vector2(-0.225f, -0.225f)) 
+            }else if(move ==  new Vector2(-1, -1)) 
             {
                 angle = -135;
-            }else if(move ==  new Vector2(-0.225f, 0.225f))
+            }else if(move ==  new Vector2(-1,1))
             {
                 angle = 135;
             }
 
 
             rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-            if((move ==  new Vector2(-0.225f,0)))
+
+            if((targetPos ==  new Vector3(-0.225f,0)))
             {
                 rotation = rotation * new Quaternion(180,0,0,0);
             }
+
+
             Instantiate(DeplacementEffectPrefab, transform.position, rotation);
+
             inMove = true;
             canMove = false;
             lockedTarget = targetPos;
+
         }
+
         if(inMove)
         {
+
             transform.position = Vector3.Lerp(transform.position, lockedTarget, (float)smoothing * Time.deltaTime);
         }
+
     }
 }
  
